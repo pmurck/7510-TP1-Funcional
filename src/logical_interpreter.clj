4cd161fe987b1ea068ca4cb6939226482010fc28
Pablo Murck <pablomurck@gmail.com>
2017-09-11 01:01:12 -0300

    Implement main method evaluate-query in logical_interpreter.

--- src/logical_interpreter.clj
+++ src/logical_interpreter.clj
@@ -1,7 +1,16 @@
-(ns logical-interpreter)
+(ns logical-interpreter
+  (:require [data-defs :refer :all] [parser :refer :all])
+  (:import (clojure.lang ExceptionInfo)))
 
 (defn evaluate-query
   "Returns true if the rules and facts in database imply query, false if not. If
   either input can't be parsed, returns nil"
-  [database query]
-  nil)
+  [database str_query]
+  (try
+    (let [[q_name q_args] (parse str_query)
+          raw_db (parse_db database)
+          db (transform_db raw_db)
+          evaluable (query db q_name (count q_args))]
+      (evaluate evaluable q_args (ref db)))
+    (catch ExceptionInfo e
+      (println (ex-data e))))) ;println devuelve nil
